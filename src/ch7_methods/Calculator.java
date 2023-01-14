10b21f2ca64972fa3fa81b0a043a3e238b0e8156
Mateusz Tadla <mat@scalepoint.com>
2023-01-14 16:41:23 +0100

    new nethod

--- src/ch7_methods/Calculator.java
+++ src/ch7_methods/Calculator.java
@@ -8,16 +8,16 @@ public class Calculator {
 
     public static int subtract(int a, int b) {
         System.out.println("I will subtract " + a + " and " + b);
-        return a + b;
+        return a - b;
     }
 
     public static int multiply(int a, int b) {
         System.out.println("I will multiply " + a + " and " + b);
-        return a + b;
+        return a * b;
     }
 
     public static int divide(int a, int b) {
         System.out.println("I will divide " + a + " and " + b);
-        return a + b;
+        return a / b;
     }
 }
