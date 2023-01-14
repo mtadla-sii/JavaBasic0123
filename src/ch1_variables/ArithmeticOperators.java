e072552c58027f393dc3ba73819d88c67bf67e9d
Mateusz Tadla <mat@scalepoint.com>
2023-01-14 09:58:49 +0100

    aryt operators

---
+++ src/ch1_variables/ArithmeticOperators.java
@@ -0,0 +1,22 @@
+package ch1_variables;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int x= 10;
+        int y = 5;
+
+        int z = 10+5;
+        int k = x+y+1;
+        System.out.println(z);
+        System.out.println(k);
+
+        System.out.println(x-2);
+        System.out.println(x*2);
+        System.out.println(x/y);
+        System.out.println(10 % 3);
+
+
+
+    }
+
+}
