cc99f7ecbb71b356ff7df0bcd2740ade16198f3e
Mateusz Tadla <mat@scalepoint.com>
2023-01-14 11:25:31 +0100

    stwith with int

---
+++ src/ch3_swtich/Ex2_SwitchWithText.java
@@ -0,0 +1,17 @@
+package ch3_swtich;
+
+import java.util.Scanner;
+
+public class Ex2_SwitchWithText {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("prosze podaj nazwę przeglądarki");
+
+        String browser = sc.nextLine();
+
+        switch (browser) {
+
+        }
+        System.out.println("Koniec programu");
+    }
+}
