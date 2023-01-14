180fe723e3a91904c197be68593bb791199c1a34
Mateusz Tadla <mat@scalepoint.com>
2023-01-14 09:47:26 +0100

    data types

---
+++ src/ch1_variables/DataTypes.java
@@ -0,0 +1,17 @@
+package ch1_variables;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        int x = 10;
+        String text = "lorem ipsum";
+        boolean isEarthFlat = false;
+
+        System.out.println("Hello world!");
+        x = 123;
+
+        int y;
+        y = 10;
+
+        System.out.println("Hello world!");
+    }
+}
