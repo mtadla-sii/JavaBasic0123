8fee0ea7974f1800a47a2304a138639dd02f17d9
Mateusz Tadla <mat@scalepoint.com>
2023-01-15 11:44:38 +0100

    oop

---
+++ src/ch9_constructor/Person.java
@@ -0,0 +1,11 @@
+package ch9_constructor;
+
+public class Person {
+    String firstName;
+    int age;
+
+    public Person(String firstName, int age) {
+        this.firstName = firstName;
+        this.age = age;
+    }
+}
